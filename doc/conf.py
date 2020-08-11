872e36c92a4cf705e3665a8dae92b986475deb73
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2020-08-11 02:46:45 +0200

    doc: remove obsolete source_parsers config option
    This is deprecated in Sphinx 3.0+ and causes `source for 'md' is already
    registered` error.

--- doc/conf.py
+++ doc/conf.py
@@ -58,11 +58,6 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-import recommonmark.parser
-source_parsers = {
-    '.markdown': recommonmark.parser.CommonMarkParser,
-}
-
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
